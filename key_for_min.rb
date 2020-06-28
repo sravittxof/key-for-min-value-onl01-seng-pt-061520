af19624a69136c79e994be211eee9c035e240751
Travis Fox <travis.fox90@gmail.com>
2020-06-28 19:42:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  running_smallest = 0
+  small_num = 
   smallest_key = ""
   if name_hash.length == 0
     return nil
