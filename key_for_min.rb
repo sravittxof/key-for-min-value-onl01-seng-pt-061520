ee595e15b314c3002602344550f09d9f70ce1c10
Travis Fox <travis.fox90@gmail.com>
2020-06-28 19:37:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  tentative_smallest = 0
+  if name_hash.length == 0
+    return nil
+  else name_hash.each do |key, value|
+    
+    
 
+    
+  end
+  
 end
